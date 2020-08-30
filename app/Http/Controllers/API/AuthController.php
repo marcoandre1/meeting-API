09d9d9f596396a6087ae9336f6407332c99171b4
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2020-08-29 22:03:03 -0400

    add controllers and routes

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return "It works!";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function signin(Request $request)
+    {
+        return "It works!";
+    }
+}
